878f9cd8be0e48f56b415a20749222149514b56f
laura-am <lauaremartin@protonmail.com>
2023-07-02 20:35:35 +0200

    update

--- hydrogen.R
+++ hydrogen.R
@@ -18,3 +18,35 @@ if (any(installed_libs == F)){
   install.packages(libs[!installed_libs])
 }
 invisible(lapply(libs, library, character.only = T))
+
+# 1. READ DATA
+#-------------
+head1 <- readxl::read_excel("Hydrogen projects database public version.xlsx",
+                            sheet = "Projects", skip = 1, col_names = T) %>%
+  names() %>%
+  str_replace("\\..\\d*", NA_character_)
+head2 <- readxl::read_excel("Hydrogen projects database public version.xlsx",
+                            sheet = "Projects", skip = 2, col_names = T) %>%
+  names() %>%
+  str_replace("\\..\\d*", NA_character_)
+
+for (n in seq_len(length(head1))) {
+  if (is.na(head2[n])) {
+    head2[n] <- head1[n]
+    head1[n] <- NA_character_
+  }
+}
+
+head1 <- tibble(head1) %>% 
+  mutate(head1 = zoo::na.locf0(head1)) %>% pull()
+head1[c(26, 31, 32)] <- NA
+
+headers <- map_chr(seq_len(length(head1)), ~{
+                     case_when(
+                       !is.na(head1[.x]) & !is.na(head2[.x]) ~ paste(head1[.x], head2[.x], sep = "_"),
+                       TRUE ~ head2[.x]
+                     )
+                   })
+
+data <- readxl::read_excel("Hydrogen projects database public version.xlsx", 
+                           sheet = "Projects", skip = 4, col_names = headers)
